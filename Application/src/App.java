0044e1ee1a2b9d47ff6d8e37feb9772c33232282
ahuss <assad.hussain@academytrainee.com>
2018-08-01 10:05:48 +0100

    feature1 added

--- Application/src/App.java
+++ Application/src/App.java
@@ -2,5 +2,17 @@
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		Account a1 = new Account("John","Doe",18567);
+		Account a2 = new Account("Jane","Doe",16758);
+		
+		AccountService.addAccount(a1);
+		AccountService.removeAccount(a2);
+		AccountService.addAccount(a2);
+		AccountService.removeAccount(a2);
+		AccountService.addAccount(a1);
+
+
+		
 	}
 }
