66eb57a67404a9841c74876b7b29364020fa28a7
ahuss <assad.hussain@academytrainee.com>
2018-08-01 10:48:14 +0100

    feature1 added fea

--- Application/src/AccountService.java
+++ Application/src/AccountService.java
@@ -2,38 +2,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AccountService {
-		private static Map<Integer,Account> accountMap = new HashMap<>();
-		
-		public static boolean addAccount(Account account ){
-			boolean success = false;
-			if(!accountMap.containsKey(account.accountNumber)) {
-				accountMap.put(account.accountNumber, account);
-				success = true;
-			}else {
-				success =false;
-			}
-			return success;
+	private static Map<Integer, Account> accountMap = new HashMap<>();
+
+	public static boolean addAccount(Account account) {
+		boolean success = false;
+		if (!accountMap.containsKey(account.accountNumber)) {
+			accountMap.put(account.accountNumber, account);
+			success = true;
+		} else {
+			success = false;
 		}
-		public static boolean removeAccount(Account account ){
-			boolean success = false;
-			if(accountMap.containsKey(account.accountNumber)) {
-				accountMap.remove(account.accountNumber);
-				success = true;
-			}else {
-				success =false;
-			}
-			return success;
+		return success;
+	}
+
+	public static boolean removeAccount(Account account) {
+		boolean success = false;
+		if (accountMap.containsKey(account.accountNumber)) {
+			accountMap.remove(account.accountNumber);
+			success = true;
+		} else {
+			success = false;
 		}
-		
-		public static boolean removeAccount(int accountNumber ){
-			boolean success = false;
-			if(accountMap.containsKey(accountNumber)) {
-				accountMap.remove(accountNumber);
-				success = true;
-			}else {
-				success =false;
-			}
-			return success;
+		return success;
+	}
+
+	public static boolean removeAccount(int accountNumber) {
+		boolean success = false;
+		if (accountMap.containsKey(accountNumber)) {
+			accountMap.remove(accountNumber);
+			success = true;
+		} else {
+			success = false;
 		}
-		
+		return success;
+	}
+
 }
